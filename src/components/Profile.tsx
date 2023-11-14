42790c0bf589ef6fbc05fec230146e5d0edd99ea
Sourin <iamsourinsarkar@gmail.com>
2023-11-15 01:35:44 +0530

    Issue with JSON file. Not resolved yet.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,9 @@
+import Data from "../data/Data.json"
+
 function Profile() {
     return (
         <div>
-            
+            <img src={Data.profile.coverPicture} />
         </div>
     );
 }
--- src/layouts/Wrapper.tsx
+++ src/layouts/Wrapper.tsx
@@ -1,9 +1,11 @@
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
+import Profile from "../components/Profile";
 
 function Wrapper() {
     return (
         <div>
-            <Navbar />
+            {/* <Navbar /> */}
+            <Profile />
         </div>
     );
 }
