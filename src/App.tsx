86b0e006e78ad7648c771df21813a898382c7325
Sourin <iamsourinsarkar@gmail.com>
2023-11-19 17:13:48 +0530

    Removed previous developments. Seemed the best wy here to organise the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Wrapper from "./layouts/Wrapper";
 
 function App() {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,9 @@
-import Logo from "../assets/images/ttt-logo.png";
-
-function Navbar() {
+const Navbar: React.FC = () => {
   return (
-    <div className="bg-[#111111]">
-      <div className="flex justify-between items-center mx-5 p-3">
+    <div>
 
-      <div>
-        <ul className="flex items-center">
-          <li className="pr-3 border-r-2 border-gray-400">
-            <img src={Logo} alt="TTT Logo" width={34} />
-          </li>
-          <li className="pl-3">
-            <span className="text-white">STORIES</span>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <button className="text-black bg-[#FEC13E] p-2 rounded-md">Courses</button>
-      </div>
-
-      </div>
     </div>
   );
 }
 
-export default Navbar;
+export default Navbar
