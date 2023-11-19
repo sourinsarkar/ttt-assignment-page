a528d9e97ba64f17321e7f87ebd79a3e978169bb
Sourin <iamsourinsarkar@gmail.com>
2023-11-19 18:24:37 +0530

    Completed Navbar Webview

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,27 @@
+import Logo from '../assets/images/ttt-logo.png'
+
 const Navbar: React.FC = () => {
   return (
-    <div>
-
+    <div className='bg-[#111111]'>
+      <div>
+        <div className='flex items-center text-white justify-between'>
+          <div>
+            <div className='flex items-center'>
+              <div className='pr-3 border-r-[1.5px] border-[#fec03f]'>
+                <img src={Logo} alt="Logo" className='h-10'/>
+              </div>
+              <div className='pl-3'>
+                <h1>STORIES</h1>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <button className='px-3 py-2 bg-[#fec03f] text-black rounded-lg'>Courses</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/layouts/Wrapper.tsx
+++ src/layouts/Wrapper.tsx
@@ -1,11 +1,9 @@
 import Navbar from "../components/Navbar";
-// import Profile from "../components/Profile";
 
 function Wrapper() {
     return (
         <div>
             <Navbar />
-            {/* <Profile /> */}
         </div>
     );
 }
