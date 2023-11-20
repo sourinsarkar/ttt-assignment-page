064c46b253546c58d4880d565818fc6554cd1265
Sourin <iamsourinsarkar@gmail.com>
2023-11-20 10:52:59 +0530

    Completed Profile: Styling to do

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,24 +1,49 @@
-import Data from '../data/Data.json'
+import importedData from '../data/Data.json'
 import { Triangle, Check } from 'iconoir-react'
 import Stats from './Stats';
+
+type ProfileType = {
+    name: string;
+    coverPicture: string;
+    profilePicture: string;
+    follower: string;
+    following: string;
+    post: string;
+    [key: string]: string;
+}
+
+const Data = {
+    profile: importedData.profile as ProfileType,
+}
+
+const toCamelCase = (str: string): string => {
+    const newstr: string = str.charAt(0).toUpperCase() + str.slice(1);
+    return newstr;
+}
+
+const followerKey: string | undefined = Object.keys(Data.profile).find(key => Data.profile[key] === Data.profile.follower) + 's';
+const followingKey: string | undefined = Object.keys(Data.profile).find(key => Data.profile[key] === Data.profile.following);
+
+const camelCaseFollowerKey = followerKey ? toCamelCase(followerKey) : undefined;
+const camelCaseFollowingKey = followingKey ? toCamelCase(followingKey) : undefined;
 
 const Profile: React.FC = () => {
     return(
         <div>
             <div>
-                <div className='h-80 bg-green-400'>
+                <div className='h-80'>
                     <img src={Data.profile.coverPicture} alt="Cover Picture" className='w-full h-full object-cover' />
                 </div>
 
                 <div className='flex items-end justify-between mt-[-5%]'>
                     <div className='w-full flex justify-center'>
-                        <div className='bg-green-400 h-40 rounded-full border-2 border-[#111111]'>
+                        <div className=' h-40 rounded-full border-2 border-[#111111]'>
                             <img src={Data.profile.profilePicture} alt="Profile Picture" className='w-full h-full rounded-inherit'/>
                         </div>
                     </div>
                     <div className='w-full flex items-start'>
                         <div>
-                            <div>
+                            <div className='mb-3'>
                                 <ul className='flex items-center'>
                                     <li>
                                         <h1 className='font-bold text-2xl'>{Data.profile.name}</h1>
@@ -28,20 +53,18 @@ const Profile: React.FC = () => {
                                 </ul>
                             </div>
 
-                            <div className='flex justify-between bg-slate-200'>
+                            <div className='flex justify-between gap-4'>
                                 <div className='w-full flex'>
-                                    <Stats value={Data.profile.follower} />
+                                    <Stats value={Data.profile.follower} statTopic={camelCaseFollowerKey} />
                                 </div>
                                 
                                 <div className='w-full flex'>
-                                    <Stats value={Data.profile.following} />
+                                    <Stats value={Data.profile.following} statTopic={camelCaseFollowingKey} />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                <div></div>
             </div>
         </div>
     );
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,11 +1,15 @@
 interface Props {
     value: string;
+    statTopic: string | undefined;
 }
 
-const Stats: React.FC<Props> = ({ value }) => {
+const Stats: React.FC<Props> = ({ value, statTopic }) => {
     return (
-        <div className="w-full flex justify-center border-2 border-black/25 rounded-xl bg-green-300">
-            <div>{value}</div>
+        <div className="w-full flex flex-col justify-center">
+            <div className="flex justify-center border-2 border-black/25 rounded-xl py-1">
+                <div className="text-xl font-bold text-black/50">{value}</div>
+            </div>
+            <div className="flex justify-center text-xs text-black/50">{statTopic}</div>
         </div>
     );
 }
